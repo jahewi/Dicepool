dc44b18c794addfee685a8a434425a6d99de5d01
Henrik Wilson <j.henrik.wilson@gmail.com>
2020-04-22 12:24:35 +0300

    Updated ToDo (now Upcoming Changes)

--- screens/DevScreen.js
+++ screens/DevScreen.js
@@ -13,18 +13,20 @@ export function DevScreen() {
     return(
         <SafeAreaView style={styles.container}>
             <ScrollView style={styles.scrollView}>
-                <Text style={styles.header}>To Do</Text>
+                <Text style={styles.header}>Upcoming Changes</Text>
                 <Text style={styles.body}>
-                    <Bullet msg="Advantage / Disadvantage selector" />
                     <Bullet msg="Savable user made roll combinations" />
                     <Bullet msg="User can make collections (characters) that include these roll combinations (actions)" />
                     <Bullet msg="Report result with modal instead of alert()" />
                     <Bullet msg="Report individual die rolls" />
+                    <Bullet msg="Advantage / Disadvantage selector" />
                     <Bullet msg="Keep a log of recent rolls" />
+                    <Bullet msg="Add ads" />
                     <Bullet msg="Select which type of game system a collection uses" />
                     <Bullet msg="Import/export collection files for backup and transfer" />
                     <Bullet msg="Make pretty" />
                     <Bullet msg="Let user create themes, aka set app colors" />
+                    <Bullet msg="Change theme according to active collection" />
                 </Text>
 
                 <Text style={styles.header}>Licenses</Text>
